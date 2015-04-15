b8f95a5b03eca615afbbb3fbedd8a29839e38327
mekilah <monroe.ekilah@gmail.com>
2015-04-15 12:04:26 -0700

    chapter 6: finish user signup flow

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,14 @@
 module UsersHelper
+
+  # Returns the Gravatar URL for the given user.
+  def gravatar_url_for(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    "https://secure.gravatar.com/avatar/#{gravatar_id}"
+  end
+
+  # Returns an image tag popuated with the Gravatar URL for the given user.
+  def gravatar_image_tag_for(user)
+    image_tag(gravatar_url_for(user), alt: user.name, class: "gravatar")
+  end
+
 end
